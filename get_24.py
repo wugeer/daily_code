8f3d4c9eab91a7f8eec6a90a19c4e40e1f2fd9e0
xiehongwang <xiehongwang@linezonedata.com>
2019-08-15 14:48:56 +0800

    [M]加了点注释

--- get_24.py
+++ get_24.py
@@ -10,6 +10,7 @@ from itertools import chain
 import random
 
 def my_all_str(num_list, char_list):
+    # 对于字符集中每一种可能，封装成下面七种形式串，并一一返回
     for item in char_list:
         resa = []
         resa.append("""({0}{4}{1}){5}{2}{6}{3}""".format(*num_list, *item))
@@ -26,8 +27,11 @@ def my_all_str(num_list, char_list):
 
 def get_all_possible(num_list):
     char_str = ['+', '-', '*', '/']*3
+    # 获取四个整数的全排列
     target_list=[list(item) for item in permutations(num_list)]
+    # 获取符号的全排列
     char_list = [list(item) for item in permutations(char_str,3)]
+    # 结果集
     res = set()
     for num_list in target_list:
         for item in my_all_str(num_list, char_list):
@@ -38,6 +42,7 @@ def get_all_possible(num_list):
                 continue
     print(res)
 if __name__ =="__main__":
+    # 获取1到10四个随机整数
     num_list = [random.randint(1,10) for i in range(4)]
     # num_list = [3,8,3,8]
     print(num_list)
